ff1dcce51d4779258447f2a51114d42ed23aa2d1
Emanuele <emanuelemastaglia@gmail.com>
2021-12-06 12:44:32 +0100

    Add 404 page

--- app/views/pages/404.php
+++ app/views/pages/404.php
@@ -1,1 +1,8 @@
-<h1><?= $data['title']?></h1>
+<?php require APPROOT.'/views/inc/header.php';?>
+<section class="section">
+    <h1 class="title has-text-white">PAGE NOT FOUND!</h1>
+    <p>Oh no the link is broken or the page has been moved. </p>
+</section>
+
+    <canvas id="404" class='c' ></canvas>
+<?php require APPROOT.'/views/inc/footer.php';?>
