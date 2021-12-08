098af2776915db1b78ed792f8c74dc5babb62613
Emanuele <emanuelemastaglia@gmail.com>
2021-12-08 19:45:26 +0100

    Update documentation

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -349,7 +349,7 @@ class Users extends Controller
                         $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
                         $mail->SMTPAuth = true; //Enable SMTP authentication
                         $mail->Username = 'emanuelemastaglia@gmail.com'; //SMTP username
-                        $mail->Password = '14078815Em'; //SMTP password
+                        $mail->Password = ''; //SMTP password
                         $mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
                         $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
